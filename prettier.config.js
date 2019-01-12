a946fe9e17313eaa786a07619e1bbfbc4d518c99
Luca Raffo <luken00@gmail.com>
2019-01-12 17:16:08 +0100

    styling and colors

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 120,
+  singleQuote: true,
+  trailingComma: "all",
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  parser: "babylon",
+  semi: true,
+  tabWidth: 4
+};
